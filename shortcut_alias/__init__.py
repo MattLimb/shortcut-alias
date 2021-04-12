6de97bb599e83d3532c6c3f67db2d5556ebd0c73
Matt Limb <matt.limb17@gmail.com>
2021-04-12 20:13:50 +0100

    Added more settings and updated output settings

--- shortcut_alias/__init__.py
+++ shortcut_alias/__init__.py
@@ -2,20 +2,30 @@ from sys import path
 import yaml
 import pathlib
 
+__author__ = "Matt Limb <matt.limb17@gmail.com>"
+
 SETTINGS = {
     "show_command": True,
     "show_reason": True,
     "show_output": True,
+    "show_output_header": True,
+    "show_skip": True,
     "colour": True
 }
 
 def load_settings(filepath):
     filepath = pathlib.Path(filepath)
-
-    with filepath.open("r") as f:
-        SETTINGS.update(
-            yaml.safe_load(f.read())
-        )
+    
+    if filepath.exists():
+        with filepath.open("r") as f:
+            SETTINGS.update(
+                yaml.safe_load(f.read())
+            )
+    else:
+        with filepath.open("w") as f:
+            f.write(
+                yaml.safe_dump(SETTINGS, indent=2)
+            )
 
 def setup_settings(filepath):
     root = pathlib.Path(filepath)
